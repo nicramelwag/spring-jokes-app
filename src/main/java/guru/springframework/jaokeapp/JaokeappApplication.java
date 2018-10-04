cebcea6f5c211ae2df3a4b6ec21084cc173ed1f9
Marcin Gawle <marcingawle@onet.pl>
2018-10-04 12:56:21 +0200

    Spring Initializr

---
+++ src/main/java/guru/springframework/jaokeapp/JaokeappApplication.java
@@ -0,0 +1,12 @@
+package guru.springframework.jaokeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaokeappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JaokeappApplication.class, args);
+    }
+}
